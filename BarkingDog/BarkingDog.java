0e569873bb49c84ec9ed503aa41344467376cc92
William Orgertrice <orgertricewill@gmail.com>
2018-06-07 14:06:26 -0400

    Added new java files

---
+++ BarkingDog/BarkingDog.java
@@ -0,0 +1,40 @@
+/*
+William Orgertrice 
+Practice java project
+ */
+package practice.java.files.BarkingDog;
+import java.util.*;
+public class BarkingDog {
+    public static void main(String[] args) {
+        Dog dog2=new Dog();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the name of the dog that's barking");
+        dog2.setname(sc.nextLine().trim());
+        System.out.println("What is the height of the dog in cm?");
+        dog2.setheight(sc.nextLine().trim());
+        bark(dog2);
+        guessheight(dog2);
+    }
+    private static void bark(Dog e){
+        System.out.println(e.getname() + " says Woof Woof");
+    }
+    private static void guessheight(Dog e){
+        System.out.println(e.getname() + " is " + e.getheight() + " cm");
+    }
+}
+class Dog{
+    String name;
+    String height;
+    public String getname(){
+        return name;
+    }
+    public void setname(String e){
+        name = e;
+    }
+    public String getheight(){
+        return height;
+    }
+    public void setheight(String e){
+        height = e;
+    }
+}
---
+++ PracticeWeightTest/PracticeWeightTest.java
@@ -0,0 +1,48 @@
+/*
+William Orgertrice 
+Practice java project
+Planet Weight Comparisons to Earth
+1.Venus 0.78
+2.Mars 0.39
+3.Jupiter 2.65
+4.Saturn 1.17
+5.Uranus 1.05
+6.Neptune 1.23
+ */
+package practice.java.files.PracticeWeightTest;
+
+import java.util.*;
+
+public class PracticeWeightTest {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How much do you weigh?");
+        double weight = sc.nextDouble();
+        System.out.println("You weigh "+weight+"ibs on Earth\n"
+                + "What planet do you want to visit?\n"
+                + "1.Venus\n"
+                + "2.Mars\n"
+                + "3.Jupiter\n"
+                + "4.Saturn\n"
+                + "5.Uranus\n"
+                + "6.Neptune");
+        String planet = sc.next().toLowerCase().trim();
+        switch(planet){
+            case "venus": weight=weight *0.78;
+            break;
+            case "mars": weight=weight *0.39;
+            break;
+            case "jupiter": weight=weight *2.65;
+            break;
+            case "saturn": weight=weight *1.17;
+            break;
+            case "uranus": weight=weight *1.05;
+            break;
+            case "neptune": weight=weight *1.23;
+            break;
+            default: System.out.println("Please enter a planet on the list of planets");
+                return;
+        }
+        System.out.println("Your weight on "+ planet + " would be " +weight+ "ib");
+    }
+}
